83687ce6d9320e4df20fc119c8514985ef295efa
wonjangcloud9 <venuscwj@naver.com>
2023-06-20 00:46:02 +0900

    잘래....

---
+++ screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
